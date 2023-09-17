78b26c864156b742d5e17c469d6b410e7720b940
MAERVIN <2211353@slu.edu.ph>
2023-09-17 15:01:20 +0800

    TO DO

--- src/Coroutine.kt
+++ src/Coroutine.kt
@@ -1,3 +1,32 @@
-fun main() {
+
+/*
+*
+*   Programmer: Maervin Villalobos
+*   Date: 09 - 17 - 2023
+*   Description: A sample problem about parking space
+*   that simulates if the car is parked
+*
+*
+*/
+
+import kotlinx.coroutines.*
+import java.util.concurrent.*
+
+class ParkingLot(capacity: Int) {
+    private val availableSlot = Semaphore(capacity)
+
+    suspend fun parkCar(cardNo: Int) {
+        try {
+            availableSlot.acquire()
+            println("Car $cardNo parked.")
+
+            delay(2000)
+        } catch (e: CancellationException) {
+
+        }
+    }
+}
+
+fun main() = runBlocking {
     print("Hello World")
 }
