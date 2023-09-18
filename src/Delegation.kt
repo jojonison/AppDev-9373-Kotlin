0606f45aa61c1729809ccdef80b97b6f4ee8449a
HAYDEE SHANE SAGUID <2211010@slu.edu.ph>
2023-09-18 17:16:37 +0800

    add Delegation.kt

---
+++ src/Delegation.kt
@@ -0,0 +1,21 @@
+interface ShowPlace {
+    fun show(placeName: String)
+}
+
+class ShowPlaceName : ShowPlace {
+    override fun show(placeName: String) {
+        print(placeName)
+    }
+}
+
+open class ShowRestaurantName(private val edit: ShowPlace) : ShowPlace by edit
+
+fun main() {
+    val showPlaceName = ShowPlaceName()
+    val showRestaurantName = ShowRestaurantName(showPlaceName)
+
+    for (i in restaurantMutableList) {
+        showRestaurantName.show(i.name)
+        println()
+    }
+}
